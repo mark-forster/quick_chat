c86e0f21631d38060e06821c6bcc379011fd52aa
mark-forster <jakeriley082@gmail.com>
2025-08-06 13:44:07 +0630

    add multer

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -8,7 +8,7 @@ const upload = multer({ storage });
 
 
 router.post('/follow/:id',isAuth, userController.followUnfollow);
-router.put('/update',isAuth, userController.updateUser);
+router.put('/update',isAuth,upload.single('profilePic'), userController.updateUser);
 router.get('/profile/:query',isAuth, userController.getUserProfile)
 router.get('/suggested', isAuth, userController.getSuggestedUsers)
 router.put('/freeze', isAuth, userController.freezeAccount)
